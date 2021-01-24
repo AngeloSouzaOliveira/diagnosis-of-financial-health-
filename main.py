426c9450578d97022dfc61dfabc15c8aed930aa5
Your Name <you@example.com>
2021-01-24 11:52:56 -0300

    Version 1.0.4

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
 import matplotlib.pyplot as graph
 
-'''
-*** a)Crie um programa que ponha a hipótese de Einstein à prova. Em uma função, receba, como entrada, um montante financeiro inicial, um percentual de rendimento por período, um valor de aporte para cada período e uma quantidade de períodos. ***
-
----> No primeiro mês, terá R$10.000 + 0,54% deste valor = R$10.054,00. E, com o novo aporte, R$11.054,00. <----
-
----> No segundo mês, o valor inicial é de R$11.054,00. Ele rende 0,54%, totalizando R$11.113,69. Com o novo aporte, R$12.113,69, e assim sucessivamente. <----
-
----> Após 2 períodos(s), o montante será de R$12113.69. <----
----> Após 3 períodos(s), o montante será de R$13179.11. <----
----> Após 4 períodos(s), o montante será de R$14250.27. <----
----> Após 5 períodos(s), o montante será de R$15327.22. <----
-
----> final: 120 meses - R$187.303,05. <----
-
----------------------------------------------------------
-
-*** b) Crie uma função que exiba um gráfico da evolução do valor acumulado, tendo como eixo das abscissas (horizontal) o número de períodos e, no eixo das ordenadas (vertical), o valor acumulado, em reais. ***
------------------------------------------------------------
-'''
 
 def aporte():
   valor_inicial = float(input("Informe o valor inicial: "))
